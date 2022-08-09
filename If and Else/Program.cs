1a22fea6069e77b0198afdd27f01dd343a47b4c1
yavuzbektas <yavuzbektas@gmail.com>
2022-08-09 15:03:00 +0300

    update

---
+++ If and Else/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_and_Else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine().Trim());
+
+
+            if (N % 2 == 1 || (N > 5 && N < 21))
+
+            { Console.WriteLine("Weird"); }
+
+            else
+            {
+                Console.WriteLine("Not Weird");
+            }
+        }
+    }
+}
--- Try_Catch/Program.cs
+++ Try_Catch/Program.cs
@@ -26,32 +26,51 @@ namespace Try_Catch
             //}
 
 
-            
-            try
-            {
-                //int a = int.Parse(null);
-                //int a = int.Parse("-2222222222");
-                int a = int.Parse("tre");
-            }
-            catch (ArgumentNullException ex)
-            {
-                Console.WriteLine("Deger bos hatası");
-                Console.WriteLine(ex);
-                
-            }
-            catch (OverflowException ex)
-            {
-                Console.WriteLine("Deger cok buyuk hatası");
-                Console.WriteLine(ex);
 
-            }
-            catch (FormatException ex)
+            //try
+            //{
+            //    //int a = int.Parse(null);
+            //    //int a = int.Parse("-2222222222");
+            //    int a = int.Parse("tre");
+            //}
+            //catch (ArgumentNullException ex)
+            //{
+            //    Console.WriteLine("Deger bos hatası");
+            //    Console.WriteLine(ex);
+
+            //}
+            //catch (OverflowException ex)
+            //{
+            //    Console.WriteLine("Deger cok buyuk hatası");
+            //    Console.WriteLine(ex);
+
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine("Format hatası");
+            //    Console.WriteLine(ex);
+
+            //}
+
+            int N = Convert.ToInt32(Console.ReadLine().Trim());
+
+
+            if (N % 2 == 0)
             {
-                Console.WriteLine("Format hatası");
-                Console.WriteLine(ex);
-
+                Console.WriteLine("Weird");
             }
-
+            else if (N >= 2 && N <= 5)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            else if (N > 6 && N <= 20)
+            {
+                Console.WriteLine("Weird");
+            }
+            else
+            {
+                Console.WriteLine("Not Weird");
+            }
 
         }
     }
