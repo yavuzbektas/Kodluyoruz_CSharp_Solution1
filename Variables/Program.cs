34fb22487d3aed636489ea0bab4cdc17fb82e6eb
yavuzbektas <yavuzbektas@gmail.com>
2022-08-09 12:29:29 +0300

    update

---
+++ Variables/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int deger1 = 0;
+            string degisken1 = "test1";
+            string degisken2 = "test2";
+            bool deger = true;
+            Int16 deger3 = 223;
+            Int32 deger4 = 222222;
+            byte deger5 = 255;
+            char deger6 = 'd';
+            double deger7 = 3.3;
+            float deger11 = 344.3f;
+            short deger8 = 4344;
+            ushort deger9 = 64769;
+            decimal deger10 = 234234.3444m;
+        }
+    }
+}
