9a5c9e32d08d5b0cdffae7958028fa959c069d1d
yavuzbektas <yavuzbektas@gmail.com>
2022-08-09 14:32:39 +0300

    update

---
+++ Try_Catch/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_Catch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //try
+            //{
+            //    Console.WriteLine("Lutfen sayı giriniz :");
+            //    int deger = Convert.ToInt16(Console.ReadLine());
+            //}
+            //catch(Exception ex)  
+            //{
+            //    Console.WriteLine("Hata : " + ex.Message.ToString());
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("İslem tamam");
+            //}
+
+
+            
+            try
+            {
+                //int a = int.Parse(null);
+                //int a = int.Parse("-2222222222");
+                int a = int.Parse("tre");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Deger bos hatası");
+                Console.WriteLine(ex);
+                
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Deger cok buyuk hatası");
+                Console.WriteLine(ex);
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Format hatası");
+                Console.WriteLine(ex);
+
+            }
+
+
+        }
+    }
+}
