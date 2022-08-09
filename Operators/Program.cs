e3bad5a344711618cfb786f88b748d48114104a4
yavuzbektas <yavuzbektas@gmail.com>
2022-08-09 12:50:29 +0300

    update

---
+++ Operators/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+
+            bool c = a > b;
+            c = a < b;
+            c = a == b;
+            c= a !=b;
+            c= a>=b;
+            c=a<= b;    
+            Console.WriteLine(c);
+
+            int say1 = 15;
+            int say2 = 5;
+            int sonuc = 0;
+
+            sonuc = say1 / say2;
+            sonuc = say1 * say2;
+            sonuc = say1 + say2;
+            sonuc = say1 - say2;
+            sonuc = say1 % say2;
+            sonuc = say1 ++;
+            Console.WriteLine(sonuc);
+        }
+    }
+}
