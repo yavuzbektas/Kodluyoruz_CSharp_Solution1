eb5777be56f10bdf4185c822c77f958a5babe27b
yavuzbektas <yavuzbektas@gmail.com>
2022-08-09 15:27:49 +0300

    update switch case

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mount = DateTime.Now.Month;
+
+            switch (mount)
+            {
+                
+                case 1:
+
+                    Console.WriteLine("Ocay Ayındasınız ");
+                    break;
+                case 2:
+                    Console.WriteLine("Subat Ayındasınız ");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart Ayındasınız ");
+                    break;
+                case 8:
+                    Console.WriteLine("Agustos Ayındasınız ");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış Veri Girişi  ");
+                    break;
+            }
+        }
+    }
+}
